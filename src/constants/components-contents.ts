e776ef33c99a37d2d350485b2fb55c784c36721a
safethecode <iam@sonjim.in>
2023-03-08 06:35:08 +0900

    chore(constants/components-contents): 푸터 내 포함될 콘텐츠 추가

---
+++ src/constants/components-contents.ts
@@ -0,0 +1,32 @@
+export const FOOTER_CONTENT = [
+  {
+    menuTitle: '약관',
+    items: [
+      {
+        title: '이용약관',
+        href: '/terms-of-use',
+      },
+      {
+        title: '개인정보처리방침',
+        href: '/privacy-policy',
+      },
+      {
+        title: '쿠키정책',
+        href: '/cookie-policy',
+      },
+    ],
+  },
+  {
+    menuTitle: '고객센터',
+    items: [
+      {
+        title: '자주묻는질문',
+        href: '/faq',
+      },
+      {
+        title: '1:1문의',
+        href: 'mailto:nullk@hey.com',
+      },
+    ],
+  },
+];
